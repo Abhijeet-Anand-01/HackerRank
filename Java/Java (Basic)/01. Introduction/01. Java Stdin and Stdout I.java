7b298b02c3ffa0c68117d980dcdc42e4e91788db
Abhijeet Anand <64329744+Abhijeet-Anand-01@users.noreply.github.com>
2021-07-31 16:08:15 +0530

    Java Stdin and Stdout I

---
+++ Java/Java (Basic)/01. Introduction/01. Java Stdin and Stdout I.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        
+        System.out.print(num1 + "\n" + num2 + "\n" + num3);
+    }
+}
