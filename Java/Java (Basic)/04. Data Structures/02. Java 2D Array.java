c9c1b80a09dc7805d9af3d17637a1ebaadbac124
Abhijeet Anand <64329744+Abhijeet-Anand-01@users.noreply.github.com>
2021-08-04 23:15:43 +0530

    Java 2D Array

--- Java/Java (Basic)/04. Data Structures/02. Java 2D Array.java
+++ Java/Java (Basic)/04. Data Structures/02. Java 2D Array.java
@@ -14,21 +14,20 @@ import static java.util.stream.Collectors.toList;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-
+        
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        
         Scanner scan = new Scanner(System.in);
         
         int arr[][] = new int[6][6];
         
         for (int row = 0; row < 6; row++) {
-            for (int col = 0; col < 6; col++) {
+            for (int col = 0; col < 6; col++) 
                 arr[row][col] = scan.nextInt();
-            }
         }
         
         scan.close();
-        
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
         
         for(int row = 0; row < 4; row++) {
             for(int col = 0; col < 4; col++) {
@@ -41,7 +40,6 @@ public class Solution {
                     max = sum;
             }
         }
-        
         System.out.println(max);      
         
     }
