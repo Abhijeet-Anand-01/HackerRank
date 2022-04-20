b4b6239b05b6127b2ca73087e23f22b321132bbb
Abhijeet Anand <abhijeetad500@gmail.com>
2022-04-20 18:35:25 +0530

    Update 02. Java If -Else.java

--- Java/Java (Basic)/01. Introduction/02. Java If -Else.java
+++ Java/Java (Basic)/01. Introduction/02. Java If -Else.java
@@ -1,19 +1,21 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        if ((n % 2 == 1) || (n >= 6 && n <= 20))
-            System.out.println("Weird");
-            
-        else if ((n % 2 == 0) || (n >= 2 && n <= 5) || (n > 20))
-                System.out.println("Not Weird");
-        
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N % 2 == 0 && ((N >= 2 && N <= 5) || N > 20))
+            System.out.println("Not Weird");
+        else System.out.println("Weird");
+        scanner.close();
     }
 }
