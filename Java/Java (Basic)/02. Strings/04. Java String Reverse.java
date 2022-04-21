cc9d6cfe06838cdf37f7739a85bf67f29a674720
Abhijeet Anand <abhijeetad500@gmail.com>
2022-04-21 19:31:28 +0530

    Update 04. Java String Reverse.java

--- Java/Java (Basic)/02. Strings/04. Java String Reverse.java
+++ Java/Java (Basic)/02. Strings/04. Java String Reverse.java
@@ -1,3 +1,5 @@
+// Approach 1 : Reverse string
+
 import java.io.*;
 import java.util.*;
 
@@ -8,7 +10,7 @@ public class Solution {
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-        
+        sc.close();
         String rev = "";
         for(int i = 0; i < A.length(); i++) 
             rev = A.substring(i, i+1) + rev;
@@ -18,5 +20,33 @@ public class Solution {
         
         else System.out.println("No");
         
+    }
+}
+
+
+
+// Approach 2 : Direct check
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        sc.close();
+        boolean flag = true;
+        int start = 0, end = A.length() - 1;
+        while (start < end) {
+            if (A.charAt(start++) != A.charAt(end--))
+                flag = false;
+        }
+        
+        if (flag)
+            System.out.println("Yes");
+        else System.out.println("No");
     }
 }
